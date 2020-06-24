37c016d47b3dc3e89fcc3f48ca37658ebcec7c2c
Zain Bin Arshad <ch.zainbinarshad@fliclicks.com>
2020-06-24 16:10:21 +0500

    added updating, creating and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,41 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Successfully created..."
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Updated Successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+        
+  end
+
 end
