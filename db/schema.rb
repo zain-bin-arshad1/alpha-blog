f1391a19f165389218895caca9b89f4fa3d2614a
Zain Bin Arshad <ch.zainbinarshad@fliclicks.com>
2020-06-25 18:09:25 +0500

    user signup form added, enhanced UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_110823) do
+ActiveRecord::Schema.define(version: 2020_06_25_113755) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_25_110823) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.text "email"
+    t.string "password_digest"
   end
 
 end
