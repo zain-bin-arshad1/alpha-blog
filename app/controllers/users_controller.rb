1de682028d38649faedb9b6a780096e7340a3d2b
Zain Bin Arshad <ch.zainbinarshad@fliclicks.com>
2020-06-25 19:36:42 +0500

    added avaatrs for suer, made layout cleaner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,30 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information is updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,6 +37,8 @@ class UsersController < ApplicationController
       render 'new' 
     end
   end
+
+
 
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def gravatar_for(user, options = { size:80 })
+  email = user.email.downcase
+  hash = Digest::MD5.hexdigest(email)
+  gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+  image_tag(gravatar_url, alt: user.username, class: "shadow rounded mx-auto d-block")  
+end
+
 end
